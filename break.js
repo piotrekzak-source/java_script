a81dac1d9850286dc7d121dd3ff2403317b8e4d5
Piotr <piotrekzak7@gmail.com>
2021-01-16 23:48:21 +0100

    break and continue

---
+++ break.js
@@ -0,0 +1,19 @@
+for (i= 0; i < 10; i++){
+
+    if( i === 5 || i === 3){
+
+        continue;
+
+    }
+
+    console.log(i);
+
+            if(i === 7){
+
+            break;
+
+
+    }
+
+}
+console.log("Wyłamałem się z pętli");
