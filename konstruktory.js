5f636d51417d4cc922b836a41ebc9b22db8a44f1
Piotr <piotrekzak7@gmail.com>
2021-01-17 15:20:17 +0100

    konstruktory

---
+++ konstruktory.js
@@ -0,0 +1,19 @@
+let Car = function(maxSpeed, driver){
+
+    this.maxSpeed = maxSpeed,
+    this.driver = driver;
+    this.drive = function(speed, time){
+
+        console.log(speed * time);
+    };
+    this.logDriver = function(){
+
+        console.log("driver name is " + this.driver);
+    }
+
+};
+let myCar = new Car(100, "James Bond");
+let myCar2 = new Car(90, "Piotr");
+
+myCar.drive(30,5);
+myCar2.logDriver();
