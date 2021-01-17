724414f9e1e7b9a1ec2787d34397ed634d690dad
Piotr <piotrekzak7@gmail.com>
2021-01-17 11:02:48 +0100

    funkcje aktualizacja

--- funkcje.js
+++ funkcje.js
@@ -1,9 +1,19 @@
 function getAvarage (a,b){
 
-var avarage = (a + b) / 2;
+var avarage = (a + b) / 2;// zmienna lokalna
 console.log(avarage);
 return avarage;
 
 }
-var Result = getAvarage(7,12);
-console.log("Srednia to"+ " " + Result);
+var Result = getAvarage(7,12);//zmienna globalna
+
+function logResult(){
+
+    console.log("Srednia to"+ "  " + Result +"  "+ "zagniezdzona w funkcji");
+
+
+}
+
+logResult();
+
+
